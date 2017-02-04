0aa28e549d1cb00a3dca462268a0cff330195e9e
Surasak P <surasak@Surasaks-Mac.local>
2017-02-04 20:55:33 +0700

    Style About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,19 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application react!</p>
+      <p>
+        Tools
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> Java script Framework
+        </li>
+        <li>
+          <a href="http://www.openweathermap.com">Open Weather Map</a>
+        </li>
+      </ul>
     </div>
   )
 };
